6a73145e17e28e37defffca44128679f90fb2fa3
MIREHMAN <Inammalik73@gmail.com>
2024-11-10 22:55:57 +0500

    Libraries Installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { Hero, Navbar,  } from "./components";
 
 const App = () => {
   return (
@@ -10,14 +10,9 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
+       
         <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
+          
         </div>
       </div>
     </BrowserRouter>
